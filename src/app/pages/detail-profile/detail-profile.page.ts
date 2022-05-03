8cd86810b90563b720bdb3d32f4f6d96ffcf64ca
Luis Vazquez <luisvazquez@MacBook-Luis.local>
2022-05-02 20:35:03 -0500

    screen user detail

--- src/app/pages/detail-profile/detail-profile.page.ts
+++ src/app/pages/detail-profile/detail-profile.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/redux/app.reducer';
+import { User } from 'src/app/redux/models/user.model';
 
 @Component({
   selector: 'app-detail-profile',
@@ -9,12 +10,21 @@ import { AppState } from 'src/app/redux/app.reducer';
 })
 export class DetailProfilePage implements OnInit {
 
+  detailData: User;
+  urlProfile: string;
+  emailDetail: string;
+
   constructor( private store: Store<AppState>,) { }
 
   ngOnInit() {
 
     this.store.select('user').subscribe( (store)=>{
-      console.log(store);
+      this.detailData = store['user']
+      console.log('this.detailData',this.detailData);
+
+      this.urlProfile = this.detailData['picture'];
+      this.emailDetail = this.detailData['email'];
+
     });
   }
 
